913b9851065d6b71db2c0292ccd6cd5418377691
Mukui012 <spencermukui@gmail.com>
2023-09-19 20:54:34 +0000

    updated module Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, ForeignKey, Table
+from sqlalchemy import Column, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 import os
 from models.place import place_amenity
