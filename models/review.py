d093096f73d9f74caf1a2dfdc1917e5412baca31
Tim Ngeno <timngeno424@gmail.com>
2023-09-19 22:43:29 +0300

    map Review module to a database table

--- models/review.py
+++ models/review.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+
+    __tablename__ = "reviews"
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"),
+                          nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"),
+                         nullable=False)
+
+        user = relationship("User", back_populates="reviews")
+        places = relationship("Place", back_populates="reviews")
+
+    else:
+        text = ""
+        place_id = ""
+        user_id = ""
