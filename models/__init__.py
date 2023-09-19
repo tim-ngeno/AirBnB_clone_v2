6fd06ac5fbd75cc600dd72edce79f8eff6fe4add
Tim Ngeno <timngeno424@gmail.com>
2023-09-19 12:19:10 +0300

    create DB storage instance

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""
+This module determines which type of storage to be used
+"""
+
 from models.engine.file_storage import FileStorage
+import os
 
+storage_type = os.getenv("HBNB_TYPE_STORAGE")
 
-storage = FileStorage()
+if storage_type == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+
+# Reload storage
 storage.reload()
