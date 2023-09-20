9268c4abf861555098bfe6ffb7b63978e6eb2bf8
Tim Ngeno <timngeno424@gmail.com>
2023-09-20 08:37:14 +0300

    patch review module

--- models/review.py
+++ models/review.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-""" Defines review class """
+""" Review module for the HBNB project """
+
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column, ForeignKey
-import os
+from sqlalchemy import Column, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class Review(BaseModel, Base):
-    """ class Review """
-    __tablename__ =  "reviews"
+    """ Review class to map Review module to a table """
 
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+    __tablename__ = "reviews"
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
         text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        place_id = Column(String(60), ForeignKey("places.id"),
+                          nullable=False)
+        user_id = Column(String(60), ForeignKey("users.id"),
+                         nullable=False)
+
+        user = relationship("User", back_populates="reviews")
+        places = relationship("Place", back_populates="reviews")
+
     else:
+        text = ""
         place_id = ""
         user_id = ""
-        text = ""
