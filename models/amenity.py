31fa79ab173663392c3cab3b38c1a45a953e5df0
Tim Ngeno <timngeno424@gmail.com>
2023-09-20 08:36:33 +0300

    add the place_amenity table

--- models/amenity.py
+++ models/amenity.py
@@ -4,7 +4,14 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey, Table
 from sqlalchemy.orm import relationship
 import os
-from models.place import place_amenity
+
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60),
+                             ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60),
+                             ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
 
 
 class Amenity(BaseModel, Base):
@@ -14,6 +21,6 @@ class Amenity(BaseModel, Base):
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         place_amenities = relationship(
-                "Place", secondary=place_amenity)
+            "Place", secondary=place_amenity)
     else:
         name = ""
