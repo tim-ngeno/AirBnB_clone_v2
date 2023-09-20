367db93e525f963b3fe873e1b580b46444c3effa
Tim Ngeno <timngeno424@gmail.com>
2023-09-20 21:56:53 +0300

    modify to_dict to remove _sa_instance_state

--- models/base_model.py
+++ models/base_model.py
@@ -88,4 +88,4 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(cls, self.id, self.to_dict())
