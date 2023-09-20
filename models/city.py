fd96f233d5dd862fce4521d8d8d9faf47ce93bfc
Tim Ngeno <timngeno424@gmail.com>
2023-09-20 13:52:11 +0300

    make table columns explicit to db storage

--- models/city.py
+++ models/city.py
@@ -5,17 +5,24 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
+import os
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(
-        String(60), ForeignKey("states.id"), nullable=False
-    )
 
-    # Create a relationship with states and places
-    state = relationship("State", back_populates="cities")
-    places = relationship("Place", back_populates="cities",
-                          cascade="all, delete")
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        state_id = Column(
+            String(60), ForeignKey("states.id"), nullable=False
+        )
+
+        # Create a relationship with states and places
+        state = relationship("State", back_populates="cities")
+        places = relationship("Place", back_populates="cities",
+                              cascade="all, delete")
+
+    else:
+        name = ""
+        state_id = ""
--- models/state.py
+++ models/state.py
@@ -4,13 +4,19 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import os
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
 
-    # Create a relationship with cities
-    cities = relationship("City", back_populates="state",
-                          cascade="all, delete, delete-orphan")
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+
+        # Create a relationship with cities
+        cities = relationship("City", back_populates="state",
+                              cascade="all, delete, delete-orphan")
+
+    else:
+        name = ""
