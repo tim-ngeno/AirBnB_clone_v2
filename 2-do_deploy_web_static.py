8876e8f1e008e5d3410863c4cb2d4effe5ffdc2e
Tim Ngeno <timngeno424@gmail.com>
2023-10-05 22:08:07 +0300

    deploy archive to servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,29 +14,19 @@ def do_deploy(archive_path):
     Distributes the archive file to the web servers
     """
     if os.path.exists(archive_path):
-        # Upload archive to /tmp/ directory
-        put(archive_path, "/tmp/")
+        archive = archive_path.split('/')[1]
+        a_path = "/tmp/{}".format(archive)
+        folder = archive.split('.')[0]
+        f_path = "/data/web_static/releases/{}/".format(folder)
 
-        # Extract filename without extension
-        basefile = os.path.basename(archive_path)
-        filename = basefile.split(".")[0]
-
-        # Create directory if it doesn't exist
-        run("mkdir -p /data/web_static/releases/{}".
-            format(filename))
-
-        # uncompress the archive to the folder on the web servers
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".
-            format(basefile, filename))
-
-        # delete the archive from the web servers
-        run("rm /tmp/{}".format(basefile))
-
-        # delete the symbolic link
+        put(archive_path, a_path)
+        run("mkdir -p {}".format(f_path))
+        run("tar -xzf {} -C {}".format(a_path, f_path))
+        run("rm {}".format(a_path))
+        run("mv -f {}web_static/* {}".format(f_path, f_path))
+        run("rm -rf {}web_static".format(f_path))
         run("rm -rf /data/web_static/current")
-
-        # create a new symbolic link
-        run("ln -s /data/web_static/releases/{} /data/web_static/current".format(filename))
+        run("ln -s {} /data/web_static/current".format(f_path))
 
         print("New version deployed!")
         return True
