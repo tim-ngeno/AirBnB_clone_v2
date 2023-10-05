324ba18db3d6563f6b9e9d581b7168f7addb3a11
Tim Ngeno <timngeno424@gmail.com>
2023-10-05 22:18:56 +0300

    deploy archive to servers -update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,9 +4,36 @@ Fabfile to deploy an archive to servers
 """
 
 from fabric.api import local, env, run, put
+from datetime import datetime
 import os
 
 env.hosts = ["18.209.152.248", "54.157.179.19"]
+
+
+def do_pack():
+    """
+    Generates a .tgz archive from the contents of `web_static`
+    """
+    # Create timestamp to append to archive name
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Archive name
+    archive_name = "web_static_{}.tgz".format(timestamp)
+
+    # Create the versions folder if it doesn't exist
+    if not os.path.exists("versions"):
+        os.mkdir("versions")
+
+    # Create the archive using local command
+    archive_path = "versions/{}".format(archive_name)
+    command = "tar -czvf {} web_static".format(archive_path)
+
+    result = local(command)
+
+    # Clean up
+    if result.failed:
+        return None
+    return archive_path
 
 
 def do_deploy(archive_path):
