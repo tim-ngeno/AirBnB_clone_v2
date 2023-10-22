4db3695ae4a17b1ccbd01421a2ea7d117e2616b2
Tim Ngeno <timngeno424@gmail.com>
2023-10-22 07:40:43 +0300

    define a /hbnb route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+"""Starting a Flask Web App"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """Displays Hello HBNB! on the URL '/' """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """Defines the HBNB URL route"""
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
