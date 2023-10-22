ad4a3b34cb27b0a71302a59aebc426d95f243409
Tim Ngeno <timngeno424@gmail.com>
2023-10-22 07:40:20 +0300

    start a flask web app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+"""Flask web app"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """
+    Defines the 'hello' URL route
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
