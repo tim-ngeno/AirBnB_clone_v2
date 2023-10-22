1d87b4de009ed0f2d3a36c509193f8be88b2de8e
Tim Ngeno <timngeno424@gmail.com>
2023-10-22 07:42:15 +0300

    display a number defined in the route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,49 @@
+"""Starting a Flask Web App"""
+from flask import Flask, abort
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """Displays Hello HBNB! on the URL '/' """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """Defines the HBNB URL route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """Defines the C URL with additional text info"""
+    if "_" in text:
+        text = text.replace("_", " ")
+    return "C {}".format(escape(text))
+
+
+@app.route("/python/", defaults={"text": "is cool"},
+           strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """Defines the python URL with additional text info"""
+    if "_" in text:
+        text = text.replace("_", " ")
+    return "Python {}".format(escape(text))
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number_route(n):
+    """Defines the number route URL"""
+    try:
+        n = int(n)
+        return "{} is a number".format(escape(n))
+    except ValueError:
+        abort(404)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
