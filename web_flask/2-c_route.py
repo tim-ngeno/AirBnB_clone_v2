aeafd5f2b4e4811956b4cb8f4e7e9d79e0224173
Tim Ngeno <timngeno424@gmail.com>
2023-10-22 07:41:24 +0300

    use a variable in the 'C' route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+"""Starting a Flask Web App"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """Displays Hello HBNB! on the URL '/' """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """Defines the HBNB URL route"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """Defines the C URL with additional text info"""
+    if "_" in text:
+        text = text.replace("_", " ")
+    return "C {}".format(escape(text))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
