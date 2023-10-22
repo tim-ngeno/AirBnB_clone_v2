d2ba24f78b3101e608864e2eebe9c42f8c7d2bfb
Tim Ngeno <timngeno424@gmail.com>
2023-10-22 17:31:45 +0300

    modify strict slashes parameter

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,9 +2,10 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def hello():
     """
     Defines the 'hello' URL route
